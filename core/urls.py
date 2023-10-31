3427137160c66b6fd96f1e00d3bb94328121a71c
System256 <roman.palcev@gmail.com>
2023-10-31 17:25:51 +0300

    fix urls views import

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from .views import *
+from .views import IndexPage
 
 
 urlpatterns = [
